37b03933b49c4bd5b7f5a02e78645ebdb5d0b304
ParthhhJoshi <parthjoshi323@gmail.com>
2026-08-19 15:27:32 +0530

    First Commit

---
+++ Lecs_Progs/3_input.py
@@ -0,0 +1,11 @@
+# how to accept input from user 
+#input 
+#variable-name = input("message")
+name = input("What is your name?")
+print(name)
+
+age = input("What is your age?")
+print("your age is",age)
+
+weight = input("What is your weight?")
+print(f"your weight is {weight}")
