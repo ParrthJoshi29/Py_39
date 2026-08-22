7c3f29d0e87ec0fa97f088be879ef8a4cd6baa91
Parth Joshi <112605114+ParrthJoshi29@users.noreply.github.com>
2026-08-22 12:20:28 +0530

    Add files via upload

---
+++ 28_Match_Case_Season.py
@@ -0,0 +1,48 @@
+# write a program to accept month number from user and display season in which month comes. month number must be valid 
+
+month = int(input("Enter Number fron 1-12: "))
+
+match month:
+    case 1:
+        print("It is Winter")
+
+    case 2:
+        print("It is Winter")
+
+    case 3:
+            print("It is Spring")
+
+    case 4:
+            print("It is Spring")
+    
+    case 5:
+            print("It is Summer")
+
+    case 6:
+            print("It is Summer")
+
+    case 7:
+            print("It is Monsoon")
+
+    case 8:
+            print("It is Monsoon")
+
+    case 9:
+            print("It is Monsoon")
+
+    case 10:
+            print("It is Autumn")
+
+    case 11:
+            print("It is Autumn")
+
+    case 12:
+            print("Winter")
+
+    case _:
+            print("Not a Valid Month")
+        
+    
+    
+    
+
